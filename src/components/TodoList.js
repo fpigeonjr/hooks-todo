8238c572fda80e8dbd14f55f80028146347b3bfe
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-01-06 14:10:18 -0600

    adds tailwind css styles

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,12 +3,32 @@ import TodosContext from '../context'
 
 export default function TodoList() {
   const { state, dispatch } = useContext(TodosContext)
+  const title =
+    state.todos.length > 0 ? `${state.todos.length} Todos` : 'No Todos!'
   return (
-    <div>
-      <ul>
+    <div className="mx-auto max-w-md text-center font-mono">
+      <h1 className="text-bold text-2xl">{title}</h1>
+      <ul className="list-reset text-white p-0">
         {state.todos.map(todo => (
-          <li key={todo.id}>
-            <span>{todo.text}</span>
+          <li
+            className="flex items-center bg-orange-700 border-black border-dashed border-2 my-2 py-4"
+            key={todo.id}
+          >
+            <span className="flex-1 cursor-pointer">{todo.text}</span>
+            <button>
+              <img
+                src="https://icon.now.sh/edit/0050c5"
+                alt="Edit Icon"
+                className="h-6"
+              />
+            </button>
+            <button>
+              <img
+                src="https://icon.now.sh/delete/8b0000"
+                alt="Delete Icon"
+                className="h-6"
+              />
+            </button>
           </li>
         ))}
       </ul>
