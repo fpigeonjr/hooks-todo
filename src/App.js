1433d1e13d12a7f6db93d40f760ed05bc54f36e7
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-01-06 13:14:09 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useReducer } from 'react'
+import { UserContext } from './index'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  count: 0
 }
 
-export default App;
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {
+        count: state.count + 1
+      }
+    case 'decrement':
+      return {
+        count: state.count - 1
+      }
+    case 'reset ':
+    default:
+      return initialState
+  }
+}
+
+export default function App() {
+  const value = useContext(UserContext)
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <div>
+      Count:{state.count}
+      <button
+        className="p-1 border"
+        onClick={() => dispatch({ type: 'increment' })}
+      >
+        Increment
+      </button>
+      <button
+        className="p-1 border"
+        onClick={() => dispatch({ type: 'decrement' })}
+      >
+        Decrement
+      </button>
+      <button
+        className="p-1 border"
+        onClick={() => dispatch({ type: 'reset' })}
+      >
+        Reset
+      </button>
+    </div>
+  )
+}
